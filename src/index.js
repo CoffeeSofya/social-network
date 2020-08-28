4c05a2cdd2442ca9ddb35731bf78a9a32be2836c
Levina Sofya <levinasof@inbox.ru>
2020-08-28 12:54:54 +0300

    packed the state and all functions in store

--- src/index.js
+++ src/index.js
@@ -2,27 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-import state, { subscribe } from './redux/State'
+import store from './redux/State'
 import App from './App';
-import {addPost, updateNewPostText, toSend, updateNewMessageText} from './redux/State';
 
 
 let rerenderEntireTree = (state) => {
   ReactDOM.render(
     <React.StrictMode>
       <App state={state} 
-      addPost={addPost}  
-      updateNewPostText={updateNewPostText}
-      toSend={toSend}
-      updateNewMessageText={updateNewMessageText} />
+      addPost={store.addPost.bind(store)}  
+      updateNewPostText={store.updateNewPostText.bind(store)}
+      toSend={store.toSend.bind(store)}
+      updateNewMessageText={store.updateNewMessageText.bind(store)} />
     </React.StrictMode>,
     document.getElementById('root')
   );
 }
 
-rerenderEntireTree(state);
+rerenderEntireTree(store.getState());
 
-subscribe(rerenderEntireTree);
+store.subscribe(rerenderEntireTree);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- src/redux/State.js
+++ src/redux/State.js
@@ -1,79 +1,174 @@
-let rerenderEntireTree = () => {
-  console.log('State changed');
-}
+let store = {
+  _state: {
+    profilePage: {
+      posts: [
+        { id: 1, message: 'Hi, how are you?', likeCount: 10 },
+        { id: 2, message: 'It is my first post!', likeCount: 20 }
+      ],
+      newPostText: ''
+    },
 
-let state = {
+    dialogsPage: {
+      dialogs: [
+        { id: 1, name: 'Sonya' },
+        { id: 2, name: 'Dima' },
+        { id: 3, name: 'Kira' },
+        { id: 4, name: 'Tema' },
+        { id: 5, name: 'Tanya' }
+      ],
+      messages: [
+        { id: 1, message: 'hi' },
+        { id: 2, message: 'hi-hi' },
+        { id: 1, message: 'hihihihi' },
+        { id: 2, message: 'hi-hi-hi' },
+        { id: 1, message: 'hihihihihihi' }
+      ],
+      newMessageText: ''
+    },
 
-  profilePage: {
-    posts: [
-      { id: 1, message: 'Hi, how are you?', likeCount: 10 },
-      { id: 2, message: 'It is my first post!', likeCount: 20 }
-    ],
-    newPostText: ''
+    sidebar: {
+      friends: [
+        { id: 1, name: 'Sonya' },
+        { id: 2, name: 'Dima' },
+        { id: 3, name: 'Kira' }
+      ]
+    }
+  },
+  getState() {
+    debugger;
+    return this._state;
+  },
+  _callSubscriber() {
+    console.log('State changed');
+  },
+  addPost() {
+    debugger;
+    let newPost = {
+      id: 5,
+      message: this._state.profilePage.newPostText,
+      likeCount: 0
+    }
+    this._state.profilePage.posts.push(newPost);
+    this._state.profilePage.newPostText = '';
+    this._callSubscriber(this._state);
+  },
+  updateNewPostText(newText) {
+    this._state.profilePage.newPostText = newText;
+    this._callSubscriber(this._state);
+  },
+  toSend() {
+    let newMessage = {
+      id: 2,
+      message: this._state.dialogsPage.newMessageText
+    }
+    this._state.dialogsPage.messages.push(newMessage);
+    this._callSubscriber(this._state);
+  },
+  updateNewMessageText(newText) {
+    this._state.dialogsPage.newMessageText = newText;
+    this._callSubscriber(this._state);
   },
 
-  dialogsPage: {
-    dialogs: [
-      { id: 1, name: 'Sonya' },
-      { id: 2, name: 'Dima' },
-      { id: 3, name: 'Kira' },
-      { id: 4, name: 'Tema' },
-      { id: 5, name: 'Tanya' }
-    ],
-    messages: [
-      { id: 1, message: 'higggggggg gggggggggggg gggggggggggg gggggggggggg gggggggggggggg gggggggggggggggggggg ggggggggggggggggggggggggggg gggggggggggg ggggggggggggggg ggggggggggggggggggg ggggggggggggggggggggggg ggggggggggggg gggggggggggggggggggg gggggggggggggggggg ggggggggggggggggggggg gggggggg' },
-      { id: 2, message: 'hi-hi' },
-      { id: 1, message: 'hihihihi' },
-      { id: 2, message: 'hi-hi-hi' },
-      { id: 1, message: 'hihihihihihi' }
-    ],
-    newMessageText: ''
-  },
-
-  sidebar: {
-    friends: [
-      { id: 1, name: 'Sonya' },
-      { id: 2, name: 'Dima' },
-      { id: 3, name: 'Kira' }
-    ]
+  subscribe(observer) {
+    this._callSubscriber = observer;
   }
 }
 
-export const addPost = () => {
-  let newPost = {
-    id: 5,
-    message: state.profilePage.newPostText,
-    likeCount: 0
-  };
-  state.profilePage.posts.push(newPost);
-  state.profilePage.newPostText = '';
-  rerenderEntireTree();
-}
-
-export const updateNewPostText = (newText) => {
-  state.profilePage.newPostText = newText;
-  rerenderEntireTree();
-}
 
 
-export const toSend = () => {
-  let newMessage = {
-    id: 2,
-    message: state.dialogsPage.newMessageText
-  };
-  state.dialogsPage.messages.push(newMessage);
-  // state.dialogsPage.newMessageText = '';
-  rerenderEntireTree();
-}
-
-export const updateNewMessageText = (newText) => {
-  state.dialogsPage.newMessageText = newText;
-  rerenderEntireTree();
-}
-
-export const subscribe = (observer) => {
-  rerenderEntireTree = observer;
-}
 
 
-export default state;
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let rerenderEntireTree = () => {
+//   console.log('State changed');
+// }
+
+// let state = {
+
+//   profilePage: {
+//     posts: [
+//       { id: 1, message: 'Hi, how are you?', likeCount: 10 },
+//       { id: 2, message: 'It is my first post!', likeCount: 20 }
+//     ],
+//     newPostText: ''
+//   },
+
+//   dialogsPage: {
+//     dialogs: [
+//       { id: 1, name: 'Sonya' },
+//       { id: 2, name: 'Dima' },
+//       { id: 3, name: 'Kira' },
+//       { id: 4, name: 'Tema' },
+//       { id: 5, name: 'Tanya' }
+//     ],
+//     messages: [
+//       { id: 1, message: 'hi' },
+//       { id: 2, message: 'hi-hi' },
+//       { id: 1, message: 'hihihihi' },
+//       { id: 2, message: 'hi-hi-hi' },
+//       { id: 1, message: 'hihihihihihi' }
+//     ],
+//     newMessageText: ''
+//   },
+
+//   sidebar: {
+//     friends: [
+//       { id: 1, name: 'Sonya' },
+//       { id: 2, name: 'Dima' },
+//       { id: 3, name: 'Kira' }
+//     ]
+//   }
+// }
+
+// export const addPost = () => {
+//   let newPost = {
+//     id: 5,
+//     message: state.profilePage.newPostText,
+//     likeCount: 0
+//   };
+//   state.profilePage.posts.push(newPost);
+//   state.profilePage.newPostText = '';
+//   rerenderEntireTree();
+// }
+
+// export const updateNewPostText = (newText) => {
+//   state.profilePage.newPostText = newText;
+//   rerenderEntireTree();
+// }
+
+
+// export const toSend = () => {
+//   let newMessage = {
+//     id: 2,
+//     message: state.dialogsPage.newMessageText
+//   };
+//   state.dialogsPage.messages.push(newMessage);
+//   rerenderEntireTree();
+// }
+
+// export const updateNewMessageText = (newText) => {
+//   state.dialogsPage.newMessageText = newText;
+//   rerenderEntireTree();
+// }
+
+// export const subscribe = (observer) => {
+//   rerenderEntireTree = observer;
+// }
+
+
+// export default state;
+export default store;
+
+window.store = store;
